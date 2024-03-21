894e849503ba4e66e6e2fe1b7f712786b657485c
spicyfalafel <285574@niuitmo.ru>
2024-03-21 22:05:21 +0300

    feat: add sidebar

--- src/viewdef_designer/index.cljs
+++ src/viewdef_designer/index.cljs
@@ -1,11 +1,9 @@
 (ns viewdef-designer.index
-  (:require [re-frame.core :as re-frame :refer [reg-event-fx subscribe reg-sub dispatch reg-event-db]]
+  (:require [re-frame.core :as re-frame :refer [reg-event-fx subscribe reg-sub dispatch]]
             [reagent.core :as r]
             [reagent.dom :as rdom]
             [suitkin.core :as ui]
             [suitkin.utils :as su]
-            [day8.re-frame.http-fx]
-            [ajax.core :as ajax]
             ; register pages
             [viewdef-designer.pages.main.view]
             [viewdef-designer.pages.view-definitions.view]
@@ -17,51 +15,24 @@
 (def compiler
   (r/create-compiler {:function-components true}))
 
-(reg-event-db
- ::got-view-definitions
- (fn [db [_ result]]
-  (assoc db
-         :view-definitions result
-         :loading false)))
-
-(reg-event-fx
-  ::get-view-definitions
-  (fn [{:keys [db]} _]
-   {:db (assoc db :loading true)
-     :http-xhrio {:method          :get
-                  :uri             "https://viewdefs1.aidbox.app/fhir/ViewDefinition"
-                  :timeout         8000
-                  :with-credentials true
-                  :headers  {:Authorization
-                             "Basic dmlldy1kZWZpbml0aW9uOnNlY3JldA=="}
-                  :response-format (ajax/json-response-format {:keywords? true})
-                  :on-success      [::got-view-definitions]
-                  :on-failure      [:bad-http-result]}}))
-
-(reg-event-fx
-  ::go-to-vd-page-and-load
-  (fn [{:keys [db]} _]
-   {:db (assoc db :loading true)
-    ::routes/navigate :vd
-    :dispatch-n [[::get-view-definitions]
-                 #_[::routes/set-active-page :vd-page]] }))
-
 (reg-sub
  ::sidebar
  (fn [_ _]
    {:menu
+
+;; {:title "Users" :img "/suitkin/img/icon/ic-users-16.svg"}
+;; {:title "SQL" :img "/suitkin/img/icon/ic-database-code-16.svg"}
     {:items
      [#_{:title "Resources"
          :img   "/suitkin/img/icon/ic-workflow-16.svg"}
 
       {:title "FHIR servers"
        :img   "/suitkin/img/icon/ic-database-16.svg"
-       :items [{:title "ViewDefinitions" :img "/suitkin/img/icon/ic-database-code-16.svg"
-                :on-click #(dispatch [::go-to-vd-page-and-load])}
+       :items [{:title "ViewDefinitions" :img "/suitkin/img/icon/ic-database-code-16.svg"}
                {:title "Resources" :img "/suitkin/img/icon/ic-table-16.svg"}]}
 
       {:divider true}
-      {:title "Settings" :img "/suitkin/img/icon/ic-users-16.svg"}
+      {:title "User" :img "/suitkin/img/icon/ic-users-16.svg"}
 
       {:title "Docs"
        :img   "/suitkin/img/icon/ic-notebook-play-16.svg"}
@@ -103,8 +74,7 @@
          :patients (->
                     (.parse js/JSON (inline-resource "mock_patients.json"))
                     (js->clj :keywordize-keys true))}
-    :fx [#_[:dispatch  [::some-event]]
-         [:dispatch [::get-view-definitions]]]}))
+    #_#_:fx [[:dispatch  [::some-event]]]}))
 
 (defn init []
   (re-frame/dispatch-sync [::initialize-db])
