a7e5a41ac68cf678271d2d38f147980b8591fd7a
Andrey Osipenko <andrey-samurai@Andreys-MacBook-Pro.local>
2024-03-22 16:49:57 +0400

    feat: WIP view definitions rendering
    Co-authored-by: @spicyfalafel <krivosheev.sviat@gmail.com>

--- src/viewdef_designer/components/table.cljs
+++ src/viewdef_designer/components/table.cljs
@@ -5,18 +5,20 @@
 (def row-class (c [:bg :white] [:border 1]))
 (def td-class (c [:px 1]))
 
-(defn table [patients]
-  (let [headers (keys (first patients))]
+(defn table [resources]
+  (let [headers (keys (first resources))]
    [:div.content
     [:table.table
-     {:class (c :w-full :text-sm :text-left :gray-500)}
+     {:class (c :w-full :text-sm
+                [:w-min 150]
+                :text-left :gray-500)}
      [:thead
       {:class (c :text-xs :gray-700 [:bg :gray-100])}
       [:tr
        (for [header headers]
         [:th header])]]
      [:tbody
-      (for [p patients]
+      (for [p resources]
        [:tr
         {:class row-class}
         (for [[_column value] p]
--- src/viewdef_designer/pages/main/model.cljs
+++ src/viewdef_designer/pages/main/model.cljs
@@ -2,6 +2,6 @@
   (:require [re-frame.core :refer [reg-sub]]))
 
 (reg-sub
- ::patients
+ ::view-definition-data
  (fn [db _]
-   (:patients db)))
+   (:view-definition/data db)))
