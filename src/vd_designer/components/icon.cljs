cc1a1f629eaeda50e6917625cb352ac23465b748
Andrey Osipenko <andrew.osipenko@health-samurai.io>
2024-03-27 19:31:17 +0400

    add(#19): optional overrides for icon components

--- src/vd_designer/components/icon.cljs
+++ src/vd_designer/components/icon.cljs
@@ -1,18 +1,19 @@
 (ns vd-designer.components.icon)
 
-(defn- icon [path]
-  [:img {:src   path
-         :style {:width  "18px"
-                 :height "18px"}}])
+(defn- icon [path overrides]
+  [:img (merge-with into {:src   path
+                          :style {:width  "18px"
+                                  :height "18px"}}
+                    overrides)])
 
-(defn column []
-  (icon "/img/form/column.svg"))
+(defn column [& {:as args}]
+  (icon "/img/form/column.svg" args))
 
-(defn foreach []
-  (icon "/img/form/forEach.svg"))
+(defn foreach [& {:as args}]
+  (icon "/img/form/forEach.svg" args))
 
-(defn union-all []
-  (icon "/img/form/unionAll.svg"))
+(defn union-all [& {:as args}]
+  (icon "/img/form/unionAll.svg" args))
 
-(defn expression []
-  (icon "/img/form/expr.svg"))
+(defn expression [& {:as args}]
+  (icon "/img/form/expr.svg" args))
