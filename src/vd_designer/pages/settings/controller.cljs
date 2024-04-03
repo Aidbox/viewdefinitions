064c483585f8f74f1473541befe27b0c90cba039
spicyfalafel <285574@niuitmo.ru>
2024-04-03 13:49:40 +0300

    fix(#27): handle empty headers
    Co-authored-by: @osipaandr <osipaandr@gmail.com>

--- src/vd_designer/pages/settings/controller.cljs
+++ src/vd_designer/pages/settings/controller.cljs
@@ -1,8 +1,9 @@
 (ns vd-designer.pages.settings.controller
   (:require
-    [lambdaisland.uri :as uri]
-    [re-frame.core :refer [inject-cofx reg-event-db reg-event-fx]]
-    [vd-designer.http.fhir-server :as http]))
+   [clojure.string :as str]
+   [lambdaisland.uri :as uri]
+   [re-frame.core :refer [reg-event-db reg-event-fx]]
+   [vd-designer.http.fhir-server :as http]))
 
 (def identifier ::main)
 
@@ -39,34 +40,46 @@
                                      {:name  k
                                       :value v}) m))))))
 
+(defn headers->map [headers]
+  (reduce
+    (fn [acc {:keys [name value]}]
+      (assoc acc (keyword name) value))
+    {} headers))
+
 (defn add-new-server [db]
-  (let [{:keys [server-name] :as new-server} (:fhir-server db)
-        new-server (update new-server :headers
-                           (fn [v]
-                             (reduce
-                               (fn [acc {:keys [name value]}]
-                                 (assoc acc (keyword name) value))
-                               {} v)))]
+  (let [{:keys [server-name] :as new-server} (:fhir-server db)]
     (if (some-> db :cfg/fhir-servers :servers not-empty)
       (update-in db [:cfg/fhir-servers :servers] merge {server-name new-server})
       (assoc db :cfg/fhir-servers {:servers          {server-name new-server}
                                    :used-server-name server-name}))))
 
 (defn edit-server [db]
-  (let [prev-server (:original-server db)
-        {:keys [server-name] :as edited-server} (-> db :fhir-server)]
+  (let [edited-server (:fhir-server db)
+        server-name (:server-name edited-server)
+        prev-server (:original-server db)
+        used-server-name (-> db :cfg/fhir-servers :used-server-name)]
     (-> db
-        (update-in [:cfg/fhir-servers :servers] dissoc (:server-name prev-server))
-        (assoc-in [:cfg/fhir-servers :servers server-name] edited-server))))
+      (update-in [:cfg/fhir-servers :servers] dissoc (:server-name prev-server))
+      (assoc-in [:cfg/fhir-servers :servers server-name] edited-server)
+      (cond->
+        (= (:server-name prev-server) used-server-name)
+        (update :cfg/fhir-servers dissoc :used-server-name)))))
+
+(defn remove-empty-headers [headers]
+  (remove
+    (fn [kv]
+      (and (str/blank? (:value kv)) (str/blank? (:name kv)))) headers))
 
 (reg-event-fx
   ::add-server
   (fn [{db :db} [_]]
-    (if (seq (:original-server db))
-      {:db (-> (edit-server db)
-               (dissoc :original-server :fhir-server))}
+    (let [update-server-list (if (seq (:original-server db))
+                               edit-server
+                               add-new-server)]
       {:db (-> db
-               (add-new-server)
+               (update-in [:fhir-server :headers]
+                          (comp headers->map remove-empty-headers))
+               (update-server-list)
                (dissoc :original-server :fhir-server))})))
 
 (reg-event-fx
@@ -77,25 +90,35 @@
      [(http/get-metadata db {:uri (-> base-url uri/uri
                                       (assoc :path "/metadata")
                                       uri/uri-str)
-                             :on-success [::connected server-name]
+                             :on-success [::get-metadata-success server-name]
                              :on-failure [::not-connected server-name]})
       (http/get-view-definitions
         db
         {:uri (-> base-url uri/uri
                   (assoc :path "/ViewDefinition")
                   uri/uri-str)
-         :headers headers
-         :on-success [::connected server-name]
+         :headers (remove
+                    (fn [[k v]]
+                      (or (str/blank? (name k)) (str/blank? (name v)))) headers)
+
+         :on-success [::get-view-definitions-success server-name]
          :on-failure [::not-connected server-name]})]}))
 
 
 (reg-event-fx
-  ::connected
-  (fn [{:keys [db local-store]} [_ server-name result]]
+  ::get-metadata-success
+  (fn [{:keys [db]} [_ server-name result]]
+    {:db (-> db
+             (assoc-in [:cfg/fhir-servers :servers server-name :fhir-version] (:fhirVersion result)))}))
+
+(reg-event-fx
+  ::get-view-definitions-success
+  (fn [{:keys [db]} [_ server-name result]]
     {:db (-> db
              (assoc-in [:cfg/fhir-servers :used-server-name] server-name)
              (assoc-in [:cfg/fhir-servers :servers server-name :fhir-version] (:fhirVersion result))
              (dissoc ::request-sent-by :edit-server :fhir-server :cfg/connect-error))}))
+
 
 (reg-event-fx
   ::not-connected
