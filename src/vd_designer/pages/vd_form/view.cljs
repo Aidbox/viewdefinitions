d8c4c6c643e9827a351090c1a32a6a683de3697f
spicyfalafel <285574@niuitmo.ru>
2024-04-08 16:10:34 +0300

    feat(#45): add table-form resizer
    Co-authored-by: @Gorovoi <derexte@gmail.com>

--- src/vd_designer/pages/vd_form/view.cljs
+++ src/vd_designer/pages/vd_form/view.cljs
@@ -1,6 +1,7 @@
 (ns vd-designer.pages.vd-form.view
   (:require ["@ant-design/icons" :as icons]
-            [antd :refer [Col Row Flex Divider]]
+            [antd :refer [Col Row]]
+            [react-resizable-panels :refer [Panel PanelGroup PanelResizeHandle]]
             [re-frame.core :refer [dispatch subscribe]]
             [reagent.core :as r]
             [vd-designer.components.button :as button]
@@ -13,35 +14,40 @@
 
 (defn viewdefinition-view []
   (let [resources @(subscribe [::m/view-definition-data])]
-    [:div {:style {:gutter 32
-                   :flex 1
-                   :display "flex"
-                   :flex-directin "row"
-                   :flex-flow "row"
-                   :overflow "hidden"}}
-     [:div {:style {:flex 1
-                    :flow-grow 1
-                    :flex-shrink 0
-                    :flex-basis "0%"
-                    :flex-direction "column"
-                    :display "flex"
-                    :override "hidden"}}
-      [button/button "Run" {:onClick #(dispatch [::c/eval-view-definition-data])
-                            :style {:max-width "80px"}}]
-      [tabs {:items [(tab-item {:key      "form"
-                                :label    "Form"
-                                :children [form]
-                                :icon     (r/create-element icons/EditOutlined)})
-                     (tab-item {:key      "yaml"
-                                :label    "YAML"
-                                :children [editor]
-                                :icon     (r/create-element icons/CodeOutlined)})]}]]
-     [:div {:style {:border-right "solid"
-                    :border-right-color "#F0F0F0"
-                    :border-width "1px"
-                    :margin-left 16
-                    :margin-right 16}}]
-     [:> Col {:span 12}
+    [:> PanelGroup {:direction "horizontal"
+                    :style {:gutter 32
+                            :flex 1
+                            :display "flex"
+                            :flex-direction "row"
+                            :flex-flow "row"
+                            :overflow "hidden"}}
+     [:> Panel 
+      {:minSize 25}
+      [:div {:style {:flex 1
+                     :flow-grow 1
+                     :flex-shrink 0
+                     :flex-basis "0%"
+                     :flex-direction "column"
+                     :display "flex"
+                     :override "hidden"
+                     :min-width "400px"}}
+                [button/button "Run" {:onClick #(dispatch [::c/eval-view-definition-data])
+                                      :style {:max-width "80px"}}]
+                [tabs {:items [(tab-item {:key      "form"
+                                          :label    "Form"
+                                          :children [form]
+                                          :icon     (r/create-element icons/EditOutlined)})
+                               (tab-item {:key      "yaml"
+                                          :label    "YAML"
+                                          :children [editor]
+                                          :icon     (r/create-element icons/CodeOutlined)})]}]]]
+     [:> PanelResizeHandle {:style {:border-right "solid"
+                                    :border-right-color "#F0F0F0"
+                                    :border-width "1px"
+                                    :margin-left 16
+                                    :margin-right 16}}]
+     [:> Panel 
+      {:minSize 20}
       [table (:data resources)
        {:scroll {:y 1000
                  :x true}}]]]))
