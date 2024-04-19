891774ac81a0ad603941c1d7e6486012e20c454e
Andrii Demydenko <andrew.demydenko@health-samurai.io>
2024-04-19 15:17:25 +0100

    fix(#73): do not allow to drop after add button

--- src/vd_designer/pages/vd_form/form.cljs
+++ src/vd_designer/pages/vd_form/form.cljs
@@ -11,15 +11,15 @@
 (defn draggable? [node]
   (let [node-key (-> (.-key node) js->clj str.utils/parse-path)]
     (cond
-      (some (set [node-key]) m/tree-root-keys) false
+      (some (set [node-key])  m/tree-root-keys) false
       ;; we do not allow to move buttons
-      (some (set [:add]) node-key) false
+      (some (set [:add])      node-key) false
       ;; there is no too much sense to move constants and where clojures, but
       ;; it'll add a css complexity to handle overlapping
       (some (set [:constant]) node-key) false
-      (some (set [:where]) node-key) false
+      (some (set [:where])    node-key) false
       ;; this is foreach nodes
-      (some (set [:path]) node-key) false
+      (some (set [:path])     node-key) false
       (= :select (peek node-key)) false
       :else true)))
 
@@ -33,11 +33,10 @@
         node-dragged?    (node? drag-key)
         drop-key         (get-key dropNode)
         dropped-in-node? (node? drop-key)]
-
     (if node-dragged?
       ;; it's node and can be moved into root select, foreach' select and union
       (not-any? (-> drop-key peek vector set)
-                [:column :forEach :forEachOrNull])
+                [:column :forEach :forEachOrNull :add])
       ;; it's column, and can be moved only into column nodes
       (if dropped-in-node?
         (= :column (-> drop-key peek))
