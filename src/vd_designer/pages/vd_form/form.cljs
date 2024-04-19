f86c8895fb685a508097df3f633ff8b3d4041050
Andrii Demydenko <andrew.demydenko@health-samurai.io>
2024-04-19 15:22:38 +0100

    refactor(#73): move drag-n-drop functions to tree

--- src/vd_designer/pages/vd_form/form.cljs
+++ src/vd_designer/pages/vd_form/form.cljs
@@ -4,43 +4,9 @@
             [vd-designer.components.tree :refer [tree] :as tree]
             [vd-designer.pages.vd-form.controller :as c]
             [vd-designer.pages.vd-form.fhir-schema :refer [create-render-context]]
-            [vd-designer.pages.vd-form.form.tree :refer [vd-tree]]
+            [vd-designer.pages.vd-form.form.tree :refer [draggable? drop-allowed? vd-tree]]
             [vd-designer.pages.vd-form.model :as m]
             [vd-designer.utils.string :as str.utils]))
-
-(defn draggable? [node]
-  (let [node-key (-> (.-key node) js->clj str.utils/parse-path)]
-    (cond
-      (some (set [node-key])  m/tree-root-keys) false
-      ;; we do not allow to move buttons
-      (some (set [:add])      node-key) false
-      ;; there is no too much sense to move constants and where clojures, but
-      ;; it'll add a css complexity to handle overlapping
-      (some (set [:constant]) node-key) false
-      (some (set [:where])    node-key) false
-      ;; this is foreach nodes
-      (some (set [:path])     node-key) false
-      (= :select (peek node-key)) false
-      :else true)))
-
-(defn drop-allowed? [info]
-  (let [{:keys [dragNode dropNode]} (js->clj info :keywordize-keys true)
-
-        get-key #(-> % :key str.utils/parse-path)
-        node?   #(-> % peek keyword?)
-
-        drag-key         (get-key dragNode)
-        node-dragged?    (node? drag-key)
-        drop-key         (get-key dropNode)
-        dropped-in-node? (node? drop-key)]
-    (if node-dragged?
-      ;; it's node and can be moved into root select, foreach' select and union
-      (not-any? (-> drop-key peek vector set)
-                [:column :forEach :forEachOrNull :add])
-      ;; it's column, and can be moved only into column nodes
-      (if dropped-in-node?
-        (= :column (-> drop-key peek))
-        (= :column (-> drop-key pop peek))))))
 
 (defn form []
   (let [vd @(subscribe [::m/current-vd])
--- src/vd_designer/pages/vd_form/form/tree.cljs
+++ src/vd_designer/pages/vd_form/form/tree.cljs
@@ -19,7 +19,9 @@
             [vd-designer.pages.vd-form.form.settings :refer [column-settings
                                                              constant-settings
                                                              where-settings]]
-            [vd-designer.utils.event :as u]))
+            [vd-designer.pages.vd-form.model :as m]
+            [vd-designer.utils.event :as u]
+            [vd-designer.utils.string :as str.utils]))
 
 ;; Leafs
 
@@ -178,3 +180,39 @@
                 (add-value-path ctx :where)    (:where    vd))
    (nested-node :select
                 (add-value-path ctx :select)   (:select   vd))])
+
+;; Drag-n-Drop
+
+(defn draggable? [node]
+  (let [node-key (-> (.-key node) js->clj str.utils/parse-path)]
+    (cond
+      (some (set [node-key])  m/tree-root-keys) false
+      ;; we do not allow to move buttons
+      (some (set [:add])      node-key) false
+      ;; there is no too much sense to move constants and where clojures, but
+      ;; it'll add a css complexity to handle overlapping
+      (some (set [:constant]) node-key) false
+      (some (set [:where])    node-key) false
+      ;; this is foreach nodes
+      (some (set [:path])     node-key) false
+      (= :select (peek node-key)) false
+      :else true)))
+
+(defn drop-allowed? [info]
+  (let [{:keys [dragNode dropNode]} (js->clj info :keywordize-keys true)
+
+        get-key #(-> % :key str.utils/parse-path)
+        node?   #(-> % peek keyword?)
+
+        drag-key         (get-key dragNode)
+        node-dragged?    (node? drag-key)
+        drop-key         (get-key dropNode)
+        dropped-in-node? (node? drop-key)]
+    (if node-dragged?
+      ;; it's node and can be moved into root select, foreach' select and union
+      (not-any? (-> drop-key peek vector set)
+                [:column :forEach :forEachOrNull :add])
+      ;; it's column, and can be moved only into column nodes
+      (if dropped-in-node?
+        (= :column (-> drop-key peek))
+        (= :column (-> drop-key pop peek))))))
