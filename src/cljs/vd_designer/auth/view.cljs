75b728558553b6e655fcac014bde3e50828d3b08
Andrii Demydenko <andrew.demydenko@health-samurai.io>
2024-05-31 09:29:37 +0100

    Revert "fix(#30): use server host from env in client"
    This reverts commit cb192288f7119c5bf49297f94e03e2853af14df4.

--- src/cljs/vd_designer/auth/view.cljs
+++ src/cljs/vd_designer/auth/view.cljs
@@ -1,11 +1,10 @@
 (ns vd-designer.auth.view
   (:require [reagent.core :as r]
-            [vd-designer.components.pop-confirm :refer [pop-confirm]]
-            [vd-designer.env :refer [SERVER_HOST]]))
+            [vd-designer.components.pop-confirm :refer [pop-confirm]]))
 
 (defn auth-required [trigger-element]
   ;; TODO: extract this url to config / env
-  (let [sso-link (str SERVER_HOST "/api/auth/sso?route=" (.-href js/window.location))]
+  (let [sso-link (str "http://localhost:8080/api/auth/sso?route=" (.-href js/window.location))]
     [pop-confirm trigger-element
      {:title       "Not authorized"
       :description (r/as-element [:p
--- src/cljs/vd_designer/env.cljs
+++
@@ -1,4 +0,0 @@
-(ns vd-designer.env)
-
-;; This is redefined in shadow-cljs.edn to set the server host from env variable.
-(goog-define SERVER_HOST "")
