9f93a5f19ef6febaa502e803fa2b8d5182cc402b
Andrii Demydenko <andrew.demydenko@health-samurai.io>
2024-05-31 12:58:42 +0100

    fix(chore): postcss config and npm cleanup
    Co-authored-by: @spicyfalafel <krivosheev.sviat@gmail.com>

--- src/cljs/postcss.clj
+++ src/cljs/postcss.clj
@@ -1,10 +1,11 @@
-(ns postcss (:require
-             [babashka.process :as proc]))
+(ns postcss
+  (:require [babashka.process :as proc]))
 
 (defn watch
   {:shadow.build/stage :configure}
   [build-state src dst]
-  (proc/process "./node_modules/.bin/postcss" src "-o" dst "--verbose" "-w"
+  (println "Watching for changes in" src)
+  (proc/process "postcss" src "--watch" "--config" "postcss.config.js" "-o" dst
                 ;; uncomment this to enable postcss for tailwind
                 #_{:extra-env {"TAILWIND_MODE" "watch"}})
   build-state)
@@ -12,6 +13,6 @@
 (defn release
   {:shadow.build/stage :configure}
   [build-state src dst]
-  (proc/process "./node_modules/.bin/postcss" src "-o" dst "--verbose"
+  (proc/process "postcss" src "--no-map" "--config" "postcss.config.js" "-o" dst
                 {:extra-env {"NODE_MODE" "production"}})
   build-state)
