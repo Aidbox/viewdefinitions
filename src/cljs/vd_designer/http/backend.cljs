b0e57752d0b470944a9784ec64af4b78db4386be
Andrii Demydenko <andrew.demydenko@health-samurai.io>
2024-06-19 14:48:00 +0100

    fix(#223): authorization header supply
    1. Do not send empty "Barrier "
    2. Display return error properly

--- src/cljs/vd_designer/http/backend.cljs
+++ src/cljs/vd_designer/http/backend.cljs
@@ -1,6 +1,10 @@
 (ns vd-designer.http.backend
   (:require [ajax.core :as ajax]))
 
+(defn authorization-header [authentication-token]
+  {:authorization (when authentication-token
+                    (str "Bearer " authentication-token))})
+
 (defn request:list-server [authentication-token]
   {:uri              "/api/aidbox/servers"
    :timeout          8000
@@ -8,4 +12,4 @@
    :response-format  (ajax/json-response-format {:keywords? true})
    :with-credentials true
    :method           :get
-   :headers          {:authorization (str "Bearer " authentication-token)}})
+   :headers          (authorization-header authentication-token)})
