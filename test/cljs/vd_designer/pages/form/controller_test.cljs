1616065a108e97fa666eb7473beafbd8790caaae
Gorovoi <derexte@gmail.com>
2024-06-24 16:00:17 +0300

    fix(#228): move test

--- test/cljs/vd_designer/pages/form/controller_test.cljs
+++ test/cljs/vd_designer/pages/form/controller_test.cljs
@@ -2,7 +2,7 @@
   (:require
    [cljs.test :refer-macros [deftest is run-test testing run-tests]]
    [matcher-combinators.test :refer [match?]]
-   [vd-designer.pages.form.controller :refer [move] :as sut]))
+   [vd-designer.pages.form.controller :refer [move collect-all-node-paths] :as sut]))
 
 (deftest move-test
   (testing "Same level leafs"
@@ -350,3 +350,88 @@
          {:where [{:path "name"}]}
          (sut/strip-empty-where-nodes {:where [{:path "name"}
                                                {:path ""}]})))))
+
+(deftest collect-all-node-paths-test
+  (testing "where, constant ignored"
+    (is
+     (=
+      #{}
+      (collect-all-node-paths {:where [{:tree/key 0}]
+                               :constant [{:tree/key 1}]}))))
+
+  (testing "one col"
+    (is
+     (=
+      #{[:select 0 :column] [:select 0] [:select]}
+      (collect-all-node-paths {:select [{:column []
+                                         :tree/key 0}]}))))
+
+  (testing "two cols"
+    (is (=
+         #{[:select 0 :column]
+           [:select 1]
+           [:select 0]
+           [:select 1 :column]
+           [:select]}
+         (collect-all-node-paths {:select [{:column [{}]
+                                            :tree/key 0}
+                                           {:column [{}]
+                                            :tree/key 1}]}))))
+
+  (testing "col in foreach"
+    (is (=
+         #{[:select 0]
+           [:select 0 :select 1 :column]
+           [:select 0 :select]
+           [:select]
+           [:select 0 :select 1]}
+         (collect-all-node-paths {:select [{:forEach "a"
+                                            :select [{:column [{}]
+                                                      :tree/key 1}]
+                                            :tree/key 0}]}))))
+  (testing "2 cols in foreach"
+    (is (=
+         #{[:select 0 :select 2]
+           [:select 0]
+           [:select 0 :select 2 :column]
+           [:select 0 :select 1 :column]
+           [:select 0 :select]
+           [:select]
+           [:select 0 :select 1]}
+         (collect-all-node-paths {:select [{:forEach "a"
+                                            :select [{:column [{}]
+                                                      :tree/key 1}
+                                                     {:column [{}]
+                                                      :tree/key 2}]
+                                            :tree/key 0}]}))))
+
+  (testing "col in foreach + col"
+    (is (=
+         #{[:select 2 :column]
+           [:select 2]
+           [:select 0]
+           [:select 0 :select 1 :column]
+           [:select 0 :select]
+           [:select]
+           [:select 0 :select 1]}
+         (collect-all-node-paths {:select [{:forEach "a"
+                                            :select [{:column [{}]
+                                                      :tree/key 1}]
+                                            :tree/key 0}
+                                           {:column [{}]
+                                            :tree/key 2}]}))))
+
+  (testing "unionAll"
+    (is (=
+         #{[:select 0 :unionAll 1 :column]
+           [:select 0 :unionAll 1]
+           [:select 0 :unionAll]
+           [:select 0]
+           [:select 0 :unionAll 2 :column]
+           [:select 0 :unionAll 2]
+           [:select]}
+         (collect-all-node-paths {:select [{:unionAll [{:column {}
+                                                        :tree/key 1}
+                                                       {:column [{}]
+                                                        :tree/key 2}]
+                                            :tree/key 0}]})))))
